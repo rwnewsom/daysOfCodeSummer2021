1d3152327d0c125421b7daae56e987c1bfd5da86
Rob Newsom <rwnewsom@gmail.com>
2021-07-21 09:01:01 -0400

    Palindrome Checker

---
+++ DaysOfCode.Tests/CheckPalindromeTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DaysOfCode.Tests
+{
+    [TestClass]
+    public class CheckPalindromeTests
+    {
+        [TestMethod]
+        public void AabaaShouldReturnTrue()
+        {
+            CheckPalindrome checkPalindrome = new CheckPalindrome();
+            string input = "aabaa";
+            bool result = checkPalindrome.isPalindrome(input);
+
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void SyzygyShouldReturnFalse()
+        {
+            CheckPalindrome checkPalindrome = new CheckPalindrome();
+            string input = "syzygy";
+            bool result = checkPalindrome.isPalindrome(input);
+
+            Assert.AreEqual(false, result);
+        }
+
+
+        [TestMethod]
+        public void AbacShouldReturnFalse()
+        {
+            CheckPalindrome checkPalindrome = new CheckPalindrome();
+            string input = "abac";
+            bool result = checkPalindrome.recursivePalindromeChecker(input);
+
+            Assert.AreEqual(false, result);
+        }
+
+        [TestMethod]
+        public void RadarShouldReturnTrue()
+        {
+            CheckPalindrome checkPalindrome = new CheckPalindrome();
+            string input = "radar";
+            bool result = checkPalindrome.recursivePalindromeChecker(input);
+
+            Assert.AreEqual(true, result);
+        }
+    }
+}
---
+++ DaysOfCode/CheckPalindrome.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DaysOfCode
+{
+    public class CheckPalindrome //TODO: make input lower case
+    {
+        public bool isPalindrome(string inputString)
+        {
+            int min = 0;
+            int max = inputString.Length - 1;
+
+            while (true)
+            {
+                if (min > max)
+                {
+                    return true;
+                }
+                char first = inputString[min];
+                char last = inputString[max];
+                if (first != last)
+                {
+                    return false;
+                }
+                min++;
+                max--;
+            }
+
+        }
+
+        public bool recursivePalindromeChecker(string inputString)
+        {
+            if (inputString.Length <= 1)
+            {
+                return true;
+            }
+            else if (inputString[0] != inputString[inputString.Length - 1])
+            {
+                return false;
+            }
+            else
+            {
+                return recursivePalindromeChecker(inputString[1..^1]);
+            }
+
+        }
+    }
+}
